99a2862b61e1b8b01978d2689489a8bd000b0025
Maksim_Gavrilov <Maksim_Gavrilov@epam.com>
2022-08-09 13:15:10 +0400

    Add tests

---
+++ src/test/java/com/epam/eco/commons/avro/modification/RemoveSchemaFieldWithRecordDefinitionTest.java
@@ -0,0 +1,55 @@
+package com.epam.eco.commons.avro.modification;
+
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaParseException;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class RemoveSchemaFieldWithRecordDefinitionTest {
+
+    private Schema getScheme() throws IOException {
+        InputStream inputStream = getClass()
+                .getClassLoader()
+                .getResourceAsStream("nested_record_with_empty_namespace.avsc");
+        return new Schema.Parser().parse(inputStream);
+    }
+
+
+    @Test
+    public void successRemove() throws Exception {
+        Schema schemaModified = SchemaModifications.of(
+                        new RemoveSchemaFieldByPath("another_two_string"),
+                        new RemoveSchemaFieldByPath("two_string")
+                )
+                .applyTo(getScheme());
+
+        assert schemaModified.getFields().size() == 1;
+        assert schemaModified.getFields().get(0).name().equals("int_field");
+
+        assert schemaModified.getFields().get(0).schema().getTypes().size() == 2;
+        assert schemaModified.getFields().get(0).schema().getTypes().get(0).getType() == Schema.Type.INT;
+        assert schemaModified.getFields().get(0).schema().getTypes().get(1).getType() == Schema.Type.NULL;
+        System.out.println(schemaModified);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void wrongOrderInRemove() throws Exception {
+        SchemaModifications.of(
+                        new RemoveSchemaFieldByPath("two_string"),
+                        new RemoveSchemaFieldByPath("another_two_string")
+                )
+                .applyTo(getScheme());
+    }
+
+    @Test(expected = SchemaParseException.class)
+    public void removeOnlyFieldWithDefinition() throws Exception {
+        SchemaModifications.of(
+                        new RemoveSchemaFieldByPath("two_string")
+                )
+                .applyTo(getScheme());
+    }
+
+
+}
